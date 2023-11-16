f667893015359e51d253f3d0b1c74a949ebaa932
Abenezer Alemayhu <alemayehuabenezer14@gmail.com>
2023-11-16 15:06:40 +0300

    link post detail css and html

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 
 
+post
+
+
 # Create your views here.
 def starting_page(request):
     return render(request, "blog/index.html")
@@ -10,5 +13,5 @@ def post(request):
     return render(request, "blog/all-post.html")
 
 
-def post_detail(request):
-    pass
+def post_detail(request, slug):
+    return render(request,"blog/post-detail.html")
