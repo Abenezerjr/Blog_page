69a7c411e118f0963a1f7de46d8feb91e1d2cf50
Abenezer Alemayhu <alemayehuabenezer14@gmail.com>
2023-11-18 13:42:06 +0300

    Add a post list

--- blog/views.py
+++ blog/views.py
@@ -1,11 +1,11 @@
 from django.shortcuts import render
 from datetime import date
 
-posts = [
+all_posts = [
     {
         "slug": "hike-in-the-mountains",
         "image": "mountains.jpg",
-        "author": "Maximilian",
+        "author": "Abenezer",
         "date": date(2023, 7, 21),
         "title": "Mountain Hiking",
         "excerpt": "There's nothing like the views you get when hiking in the mountains! And I wasn't even prepared "
@@ -26,11 +26,12 @@ posts = [
     },
     {
         "slug": "programming-is-fun",
-        "image": "coding.jpg",
-        "author": "Maximilian",
+        "image": "code.png",
+        "author": "Abenezer",
         "date": date(2022, 3, 10),
         "title": "Programming Is Great!",
-        "excerpt": "Did you ever spend hours searching that one error in your code? Yep - that's what happened to me yesterday...",
+        "excerpt": "Did you ever spend hours searching that one error in your code? "
+                   "Yep - that's what happened to me yesterday...",
         "content": """
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis nobis
           aperiam est praesentium, quos iste consequuntur omnis exercitationem quam
@@ -47,8 +48,8 @@ posts = [
     },
     {
         "slug": "into-the-woods",
-        "image": "woods.jpg",
-        "author": "Maximilian",
+        "image": "code.png",
+        "author": "Abenezer",
         "date": date(2022, 8, 5),
         "title": "Nature At Its Best",
         "excerpt": "Nature is amazing! The amount of inspiration I get when walking in nature is incredible!",
@@ -69,14 +70,28 @@ posts = [
 ]
 
 
+def get_date(post):
+    return post['date']
+
+
 # Create your views here.
 def starting_page(request):
-    return render(request, "blog/index.html")
+    sorted_posts = sorted(all_posts, key=get_date)
+    latest_posts = sorted_posts[-3:]
+
+    return render(request, "blog/index.html", {
+        "posts": latest_posts
+    })
 
 
 def post(request):
-    return render(request, "blog/all-post.html")
+    return render(request, "blog/all-post.html", {
+        "all_post": all_posts
+    })
 
 
 def post_detail(request, slug):
-    return render(request, "blog/post-detail.html")
+    identified_post=next(post for post in all_posts if post['slug'] == slug)
+    return render(request, "blog/post-detail.html",{
+        "post":identified_post
+    })
