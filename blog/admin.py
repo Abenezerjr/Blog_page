59a5104cda41204a061db59b07cfcb82ecd1ba28
Abenezer Alemayhu <alemayehuabenezer14@gmail.com>
2023-11-27 23:42:13 +0300

    Enhance Post model admin view with list filters and display fields

--- blog/admin.py
+++ blog/admin.py
@@ -1,10 +1,14 @@
 from django.contrib import admin
 
+
 # Register your models here.
+class PostAdmin(admin.ModelAdmin):
+    list_filter = ("author", "tag", "date",)
+    list_display = ("title", "date", "author",)
 
 
 from .models import Post, Author, Tag
 
-admin.site.register(Post)
+admin.site.register(Post, PostAdmin)
 admin.site.register(Author)
 admin.site.register(Tag)
