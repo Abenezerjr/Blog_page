eb07ee307260d6a8879561c546a0347c4dce7a22
Abenezer Alemayhu <alemayehuabenezer14@gmail.com>
2023-11-28 00:10:07 +0300

    Refactor views.py to fetch all posts and specific post details from a Post model

--- blog/views.py
+++ blog/views.py
@@ -1,19 +1,11 @@
-from django.shortcuts import render
-from datetime import date
-from . models import Post
+from django.shortcuts import render, get_object_or_404
 
-all_posts = [
-
-]
-
-
-def get_date(post):
-    return post['date']
+from .models import Post
 
 
 # Create your views here.
 def starting_page(request):
-    latest_posts=Post.objects.all().order_by("-date")[:3]
+    latest_posts = Post.objects.all().order_by("-date")[:3]
     # sorted_posts = sorted(all_posts, key=get_date)
     # latest_posts = sorted_posts[-3:]
 
@@ -23,13 +15,15 @@ def starting_page(request):
 
 
 def post(request):
+    all_posts = Post.objects.all().order_by("-date")
     return render(request, "blog/all-post.html", {
         "all_post": all_posts
     })
 
 
 def post_detail(request, slug):
-    identified_post = next(post for post in all_posts if post['slug'] == slug)
+    identified_post = get_object_or_404(Post, slug=slug)
+    # identified_post = next(post for post in all_posts if post['slug'] == slug)
     return render(request, "blog/post-detail.html", {
         "post": identified_post
     })
