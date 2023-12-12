8b945c588948f6c1e2fcfd70f2f0a9065c37ef94
Abenezer Alemayhu <alemayehuabenezer14@gmail.com>
2023-12-12 09:14:57 +0300

    Refactor SinglePostView to handle GET and POST requests separately

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,9 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
+from django.urls import reverse
 from django.views.generic import ListView, DetailView
 from .models import Post
+from django.views import View
 from .form import CommentForm
 
 
@@ -39,15 +42,41 @@ class AllPostsView(ListView):
 #         "all_post": all_posts
 #     })
 
-class SinglePostView(DetailView):
-    template_name = "blog/post-detail.html"
-    model = Post
+class SinglePostView(View):
+    # template_name = "blog/post-detail.html"
+    # model = Post
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context["post_tags"] = self.object.tag.all()
-        context["comment_form"]=CommentForm
-        return context
+    def get(self, request, slug):
+        post = Post.objects.get(slug=slug)
+        context = {
+            "post": post,
+            "post_tags": post.tag.all(),
+            "comment_form": CommentForm()
+        }
+
+        return render(request, "blog/post-detail.html", context)
+
+    def post(self, request, slug):
+        comment_form = CommentForm(request.POST)
+        post = Post.objects.get(slug=slug)
+        if comment_form.is_valid():
+            comment = comment_form.save(commit=False)
+            comment.post = post
+            comment.save()
+
+            return HttpResponseRedirect(reverse("post-detail-page", args=[slug]))
+        context = {
+            "post": post,
+            "post_tags": post.tag.all(),
+            "comment_form": comment_form
+        }
+        return render(request, "blog/post-detail.html", context)
+
+    # def get_context_data(self, **kwargs):
+    #     context = super().get_context_data(**kwargs)
+    #     context["post_tags"] = self.object.tag.all()
+    #     context["comment_form"]=CommentForm
+    #     return context
 # def post_detail(request, slug):
 #     identified_post = get_object_or_404(Post, slug=slug)
 #     # identified_post = next(post for post in all_posts if post['slug'] == slug)
